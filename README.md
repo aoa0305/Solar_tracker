f2693c4a8dcbe8a2881c35524f94fd98cdcbaef3
aoa0305 <olamijiakindele@gmail.com>
2020-02-06 18:12:46 -0500

    Solar Tracker
    Arduino UNO project that uses a solar panel connected to servomotors and to track the best position for most light absorption.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Arduino
+
+Arduino UNO project that uses a solar panel connected to servomotors and to track the best position for most light absorption.
+Link to Projects Explanation/Execution - https://vimeo.com/265932517
---
+++ Solar_tracker_1.ino
@@ -0,0 +1,74 @@
+# include <Servo.h>
+Servo servo1;
+Servo servo2;
+int pos1=0;
+int pos2=0;
+int up=0;
+int down=0;
+int right=0;
+int left=0;
+int centre=0;
+int ldr1=0;
+int ldr2=0;
+int ldr3=0;
+int ldr4=0;
+int ldr5=0;
+ 
+void setup ()
+{
+servo1.attach(10);
+servo1.write(90);
+servo2.attach(9);
+servo2.write(90);
+pinMode(ldr1, A0);
+pinMode(ldr2, A1);
+pinMode(ldr3, A2);
+pinMode(ldr4, A3);
+pinMode(ldr5, INPUT);
+}
+ 
+void loop ()
+{
+pos1=servo1.read();
+pos2=servo2.read();
+int up= analogRead(ldr1);
+int down= analogRead(ldr2);
+int centre= analogRead(ldr3);
+int right= analogRead(ldr4);
+int left= analogRead(ldr5);
+ 
+//for conrol of vertical i.e. up-down(east-west) position
+  if (up>centre << down<centre)
+  {
+servo1.write(pos1+1);
+delay(10);
+}
+else if (down>centre << up<centre)
+  {
+servo1.write(pos1-1);
+delay(10);
+}
+else
+{
+servo1.write(pos1);
+delay(10);
+}
+ 
+ //for control of horizontal i.e.right-left(south-north) position
+ 
+if (right>centre << left<centre)
+  {
+servo2.write(pos2+1);
+delay(10);
+}
+else if (left>centre << right<centre)
+  {
+servo2.write(pos2-1);
+delay(10);
+}
+else
+  {
+servo2.write(pos2);
+delay(10);
+}
+}
